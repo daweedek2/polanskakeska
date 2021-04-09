40ae36a0d7be4ba7a1c5e6ab1316426d32a26c4f
David Kostka <dkostka@eurofunk.com>
2021-04-09 20:51:02 +0200

    Fix validation of uppercase answers

--- src/main/java/cz/kostka/polanskakeska/utils/AnswerUtil.java
+++ src/main/java/cz/kostka/polanskakeska/utils/AnswerUtil.java
@@ -1,6 +1,7 @@
 package cz.kostka.polanskakeska.utils;
 
 import java.text.Normalizer;
+import java.util.Locale;
 
 public class AnswerUtil {
 
@@ -11,6 +12,7 @@ public class AnswerUtil {
     public static String normalize(final String answer) {
         return Normalizer
                 .normalize(answer, Normalizer.Form.NFD)
-                .replaceAll("[^\\p{ASCII}]", "");
+                .replaceAll("[^\\p{ASCII}]", "")
+                .toLowerCase(Locale.ROOT);
     }
 }
